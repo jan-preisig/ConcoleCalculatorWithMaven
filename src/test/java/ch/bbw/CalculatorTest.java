1633e717c0c3966c7c69b93da031c952f2412732
jan_preisig <jan_preisig@gmx.net>
2018-12-22 15:41:07 +0100

    prepare test for subtraktion

--- src/test/java/ch/bbw/CalculatorTest.java
+++ src/test/java/ch/bbw/CalculatorTest.java
@@ -13,4 +13,10 @@ public class CalculatorTest {
     calculator = new Calculator();
     assertEquals(25, calculator.summe(10, 15));
   }
+
+  @Test
+  public void testSubtraktionZweiPositiveIsOk() {
+    calculator = new Calculator();
+    assertEquals(5, calculator.subtraktion(15,10));
+  }
 }
